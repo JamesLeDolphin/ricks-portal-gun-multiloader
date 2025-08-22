d0a19c16bdc89ca039311953a403e08e82208161
JamesLeDolphin <karl.kallip@gmail.com>
2025-08-22 16:03:04 +0300

    register unregged packets on neo

--- neoforge/src/main/java/com/jdolphin/ricksportalgun/common/init/NeoForgePackets.java
+++ neoforge/src/main/java/com/jdolphin/ricksportalgun/common/init/NeoForgePackets.java
@@ -28,6 +28,8 @@ public class NeoForgePackets {
         registrar.commonToServer(SBWorkbenchWaypointEditPackage.ID, SBWorkbenchWaypointEditPackage.CODEC, NeoForgePackets::handle);
         registrar.commonToServer(SBSetPortalGunTypePacket.ID, SBSetPortalGunTypePacket.CODEC, NeoForgePackets::handle);
         registrar.commonToServer(SBActivateSelfDestructPacket.ID, SBActivateSelfDestructPacket.CODEC, NeoForgePackets::handle);
+        registrar.commonToServer(SBSetDispenserDestinationPacket.ID, SBSetDispenserDestinationPacket.CODEC, NeoForgePackets::handle);
+        registrar.commonToServer(SBSetBarrierCodePacket.ID, SBSetBarrierCodePacket.CODEC, NeoForgePackets::handle);
 
         //Client bound
         registrar.commonToClient(CBOpenCoordGuiPacket.ID, CBOpenCoordGuiPacket.CODEC, (packet, context) -> ClientPacketHandler.openCoordTravelScreen(packet.strings()));
