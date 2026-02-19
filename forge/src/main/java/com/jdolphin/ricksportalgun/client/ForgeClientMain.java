1edef8f241d5fec3487ff254b4c30df6b40602ce
JamesLeDolphin <karl.kallip@gmail.com>
2026-02-19 19:00:05 +0200

    render type for that fluid  tank thing

--- forge/src/main/java/com/jdolphin/ricksportalgun/client/ForgeClientMain.java
+++ forge/src/main/java/com/jdolphin/ricksportalgun/client/ForgeClientMain.java
@@ -3,9 +3,12 @@ package com.jdolphin.ricksportalgun.client;
 import com.jdolphin.ricksportalgun.PGConstants;
 import com.jdolphin.ricksportalgun.client.event.PGClientEventHandler;
 import com.jdolphin.ricksportalgun.client.init.*;
+import com.jdolphin.ricksportalgun.common.init.PGBlocks;
 import com.jdolphin.ricksportalgun.common.init.PGKeyBinds;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RegisterColorHandlersEvent;
@@ -34,9 +37,10 @@ public class ForgeClientMain {
             event.register(PGTintHandler::tint, PGTintHandler.TINTABLES);
         }
 
-        @SuppressWarnings({"rawtypes", "unchecked"})
+        @SuppressWarnings({"rawtypes", "unchecked", "removal"})
         @SubscribeEvent
         public static void clientSetup(FMLClientSetupEvent event) {
+            ItemBlockRenderTypes.setRenderLayer(PGBlocks.PORTAL_FLUID_TANK, RenderType.translucent());
             event.enqueueWork(() -> {
                 PGMenuScreens.ALL.forEach((type, func) -> {
                     MenuScreens.ScreenConstructor constructor = func::apply;
