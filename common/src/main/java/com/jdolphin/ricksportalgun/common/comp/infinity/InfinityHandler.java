bc8a9dd292c7f11ed86996923f1fc1c2b575ab5d
JamesLeDolphin <karl.kallip@gmail.com>
2026-02-24 20:53:43 +0200

    fix infinity compat

--- common/src/main/java/com/jdolphin/ricksportalgun/common/comp/infinity/InfinityHandler.java
+++ common/src/main/java/com/jdolphin/ricksportalgun/common/comp/infinity/InfinityHandler.java
@@ -2,22 +2,15 @@ package com.jdolphin.ricksportalgun.common.comp.infinity;
 
 import net.lerariemann.infinity.util.InfinityMethods;
 import net.lerariemann.infinity.util.teleport.PortalCreator;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.level.Level;
 
 public class InfinityHandler {
 
-    public static ResourceKey<Level> getOrCreateResourceKey(MinecraftServer server, String dim) {
+    public static ResourceLocation getDimensionId(MinecraftServer server, String dim) {
         ResourceLocation id = InfinityMethods.dimTextToId(dim);
         PortalCreator.recordIdTranslation(server, id, dim);
-        ResourceKey<Level> levelKey = ResourceKey.create(Registries.DIMENSION, id);
-
-        if (server.getLevel(levelKey) == null) {
-            server.executeIfPossible(() -> PortalCreator.tryAddInfinityDimension(server, id));
-        }
-        return levelKey;
+        server.executeIfPossible(() -> PortalCreator.tryAddInfinityDimension(server, id));
+        return id;
     }
 }
