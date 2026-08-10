80c6c9ff9a9bd4302ddd61109f55a8ade018b258
JamesLeDolphin <karl.kallip@gmail.com>
2026-08-10 15:42:20 +0300

    edit PortalEntity constructor to allow subclasses

--- common/src/main/java/com/jdolphin/ricksportalgun/common/entity/PortalEntity.java
+++ common/src/main/java/com/jdolphin/ricksportalgun/common/entity/PortalEntity.java
@@ -66,7 +66,7 @@ public class PortalEntity extends Entity {
         return exists;
     }
 
-    public PortalEntity(EntityType<PortalEntity> type, Level level) {
+    public PortalEntity(EntityType<? extends PortalEntity> type, Level level) {
         super(type, level);
     }
 
